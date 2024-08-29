e8560aa8a254f3825af9d1d06ce29127caa45f47
Josiah Connors <josiah.connors23@outlook.com>
2024-08-29 00:22:43 -0400

    validation schema updated

--- transactions_api/app.py
+++ transactions_api/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from schema import Schema, And, Use, Optional, SchemaError
 from jsonschema import validate, ValidationError
 import json 
 
@@ -14,43 +15,32 @@ def receive_data():
     # Get the JSON data from the request
     json_data = request.get_json()
     # input validation 
-    validate_data(json_data)
+    # validate_data(json_data)
+    validate_transactions(json_data)
     # Save the JSON data to a file
     with open('received_data.json', 'w') as file:
         json.dump(json_data, file, indent=4)
     return jsonify({"message": "Transaction data received and saved successfully"}), 200
 
 
-def validate_data(data):
-    try: 
-        # Define the schema for the JSON data
-        schema = {
-            "type": "object",
-            "properties": {
-                "accountNumber": {
-                    "type": "number",
-                    "description": "The user's account number"
-                },
-                "amount": {
-                    "type": "number",
-                    "description": "monetary value of transaction"
-                },
-                "vendor": {
-                    "type": "string",
-                    "description": "merchant"
-                },
-                "timestamp": {"type": "string",
-                              "format": "date-time",
-                              "description": "time of transaction"
-                }
-            },
-            "required": ["accountNumber", "amount", "vendor", "timestamp"]
-        }
-        # Validate the JSON data against the schema
-        validate(instance=data, schema=schema)
-    except ValidationError as e:
-        raise Exception(f"Invalid JSON data: {e.message}")
+# Define the schema for a single transaction
+transactions_schema = Schema({
+    'accountNumber': And(int, lambda n: n > 0),
+    'amount': And(float, lambda f: f >= 0),
+    'vendor': str,
+    'timestamp': And(str, Use(lambda s: s.endswith('Z')))
+})
+
+
+def validate_transactions(transactions):
+    try:
+        transactions_schema.validate(transactions)
+        print("Validation successful!")
+    except SchemaError as e:
+        raise ValueError(f"Validation error: {e}")
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5001)
+
+
