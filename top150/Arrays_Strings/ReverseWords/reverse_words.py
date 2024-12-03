ea8c7583231fabfef0b2d748b2a40194f9139769
Josiah Connors <josiah.connors23@outlook.com>
2024-12-03 15:18:05 -0500

    reverse words

--- top150/Arrays_Strings/ReverseWords/reverse_words.py
+++ top150/Arrays_Strings/ReverseWords/reverse_words.py
@@ -2,33 +2,43 @@ class Solution(object):
 
     def reverseWords(self, s):
         stripped_s = s.strip()
-        print(f"strip: {stripped_s}")
-        rev_s = stripped_s[::-1]
-        print(f"reversed: {rev_s}")
+        clean_s = ' '.join(stripped_s.split())
+        rev_s = clean_s[::-1]
         words = ''
         track = 0
+        last_space = 0
         for i in range(len(rev_s)):
             if rev_s[i] == ' ':
-                print("reached")
+                last_space = i
                 word = rev_s[track: i]
-                (f"word to reverse: {word}, i: {i}")
                 rev_word = word[::-1]
                 if track == 0:
                     word = rev_s[track: i]
-                    (f"word to reverse: {word}, i: {i}")
                     rev_word = word[::-1]
                     words += rev_word
-                    print(words)
                     track = i+1
                     continue
                 else: 
-                    print("reached")
                     words += " " 
                     track = i+1
                     words += rev_word
-                    print(words)
-            print(f"tracker: {track}")
+            elif not rev_s[i] == ' ' and i == len(rev_s)-1:
+                if last_space == 0:
+                    words += clean_s 
+                    return words
+                words += " " 
+                word = rev_s[last_space+1:]
+                rev_word = word[::-1]
+                words += rev_word
+        
         return words
 
 solution_instance = Solution() 
-print(solution_instance.reverseWords(s="  hello world  "))
+
+
+# print(solution_instance.reverseWords(s="  hello world  "))
+# print(solution_instance.reverseWords(s="the sky is blue f     "))
+# print(solution_instance.reverseWords(s="a good   example"))
+print(solution_instance.reverseWords(s="EPY2giL"))
+# print(f"cur char: {rev_s[i]}")
+# print(f"word to reverse: {word}, i: {i}")
